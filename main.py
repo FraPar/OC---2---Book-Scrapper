b7933770430575513b26bc19bf336ba2cf3aadd0
Francois Parenti <francois.parenti@gmail.com>
2021-06-14 11:20:25 +0200

    getting all books of the site

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://books.toscrape.com/')
-
-html_doc = r.text
-
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-all_book = soup.findAll("li", {"class":"col-xs-6 col-sm-4 col-md-3 col-lg-3"})
-
 books = []
-for book in all_book:
-    content = book.find('h3')
-    data = content.a
-    title = data.get('title')
-    books.append(title)
 
-lentghbooks = len(books)
+for i in range(51):
+    r = requests.get('https://books.toscrape.com/catalogue/page-'+str(i)+'.html')
+
+    html_doc = r.text
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    all_book = soup.findAll("li", {"class":"col-xs-6 col-sm-4 col-md-3 col-lg-3"})
+
+    for book in all_book:
+        content = book.find('h3')
+        data = content.a
+        title = data.get('title')
+        books.append(title)
+
+    lentghbooks = len(books)
 
 print("There are", lentghbooks, "books :")
 print(books)
